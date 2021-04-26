a6d54906a2154e5a03b0a15f3ffe4a8ac5253497
杨莉 <feihan177@sina.com>
2021-04-26 14:29:49 +0800

    Delete docs directory

--- docs/.vuepress/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  title: "悦目是佳人",
-  description: "Just playing around",
-  head: [
-    ['link', { rel: 'icon', href: '/logo.png' }]
-  ]
-};
--- docs/README.md
+++
@@ -1,4 +0,0 @@
-
-![logo](https://www.jiuwa.net/tools/jupai/temp/1619316744.png "首页标题")
-
-#
